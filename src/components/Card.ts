a86a693446b8a69e71ccaf66c13c2815b74cce84
boshan <ziijey@yandex.ru>
2024-12-17 03:33:13 +0300

    tune Card

--- src/components/Card.ts
+++ src/components/Card.ts
@@ -54,15 +54,12 @@ export class Card extends Component<ICard> {
 
 export class BasketCard extends Card {
 	protected _index: HTMLElement;
-	protected _title: HTMLElement;
 	protected _deleteButton: HTMLButtonElement;
 
 	constructor(container: HTMLElement, actions?: ICardActions) {
 		super(container);
 
 		this._index = ensureElement<HTMLElement>(`.basket__item-index`, container);
-		this._title = ensureElement<HTMLElement>(`.card__title`, container);
-		this._price = ensureElement<HTMLElement>(`.card__price`, container);
 		this._deleteButton = ensureElement<HTMLButtonElement>(`.basket__item-delete`,container);
 		if (actions && actions.onClick) {
 			this._deleteButton.addEventListener('click', actions.onClick);
